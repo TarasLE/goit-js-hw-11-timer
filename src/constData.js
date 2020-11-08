ac560922578aa6fd890c47cdfb6cef3ee44bf31e
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-08 13:05:17 +0200

    interim save

---
+++ src/constData.js
@@ -0,0 +1,4 @@
+export const days = Math.floor(time / (1000 * 60 * 60 * 24));
+export const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+export const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+export const secs = Math.floor((time % (1000 * 60)) / 1000);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './styles.css';
+import { days } from './constData';
+import { hours } from './constData';
+import { mins } from './constData';
+import { secs } from './constData';
+
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2019'),
+});
