c4afb529e6dd9b1969a05dfdceb47f370c2f1898
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-08 13:38:11 +0200

    interim save

--- src/constData.js
+++ src/constData.js
@@ -1,5 +1,5 @@
-export const time = targetdate - Date.now;
-export const days = Math.floor(time / (1000 * 60 * 60 * 24));
-export const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-export const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-export const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+ const days = Math.floor(time / (1000 * 60 * 60 * 24));
+ const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+ const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+ const secs = Math.floor((time % (1000 * 60)) / 1000);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import { days } from './constData';
-import { hours } from './constData';
-import { mins } from './constData';
-import { secs } from './constData';
+// import { days } from './constData';
+// import { hours } from './constData';
+// import { mins } from './constData';
+// import { secs } from './constData';
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jul 17, 2019'),
 });
+
+
+
+const time = targetDate - Date.now();
+ const days = Math.floor(time / (1000 * 60 * 60 * 24));
+ const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+ const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+ const secs = Math.floor((time % (1000 * 60)) / 1000);
