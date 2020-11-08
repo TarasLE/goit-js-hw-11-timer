75b8c4a7e70ff2e02a33d9f1e166969facb3937e
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-08 13:33:20 +0200

    interim save

--- src/constData.js
+++ src/constData.js
@@ -1,3 +1,4 @@
+export const time = targetdate - Date.now;
 export const days = Math.floor(time / (1000 * 60 * 60 * 24));
 export const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 export const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
