708cb53f45203a7e77d81f411b66efcd9965c3c1
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-08 17:05:12 +0200

    interim save

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-// import { days } from './constData';
-// import { hours } from './constData';
-// import { mins } from './constData';
-// import { secs } from './constData';
+import './styles.css';
+
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+
+
+        this.refs = {
+        dispDays: document.querySelector(`${this.selector} [data-value="days"]`),
+        dispHours: document.querySelector(`${this.selector} [data-value="hours"]`),
+        dispMins: document.querySelector(`${this.selector} [data-value="mins"]`),
+        dispSecs: document.querySelector(`${this.selector} [data-value="secs"]`),
+      }
+    }
+    
+    
+
+      startTimer() {
+          setInterval(() => {
+                const currentTime = Date.now();
+                const time = this.targetDate - currentTime;
+                const {days, hours, mins, secs} = getTimeComponents(time)
+              console.log(`${ days }days ${ hours }:${ mins }:${secs}`);
+              this.updateTimerFace(getTimeComponents(time));
+           
+            },1000)
+    }
+    updateTimerFace({ days, hours, mins, secs }) {
+        this.refs.dispDays.innerHTML = days;
+        this.refs.dispHours.innerHTML = hours;
+        this.refs.dispMins.innerHTML = mins;
+        this.refs.dispSecs.innerHTML = secs;
+    }
+}
+ 
+
+function getTimeComponents(time){
+ const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+ const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+ const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+ const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    
+    return{days, hours, mins, secs}
+}
+    
+function pad(value) {
+    return String(value).padStart(2, '0');
+    
+}
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date('Nov 9, 2020'),
 });
 
 
-
-const time = targetDate - Date.now();
- const days = Math.floor(time / (1000 * 60 * 60 * 24));
- const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
- const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
- const secs = Math.floor((time % (1000 * 60)) / 1000);
+timer.startTimer();
