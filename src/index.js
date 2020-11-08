19587a55ba01fe4607a0b91f8ae2742f63c7dfb8
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-08 13:18:52 +0200

    interim save

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { hours } from './constData';
 import { mins } from './constData';
 import { secs } from './constData';
 
-new CountdownTimer({
+const timer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jul 17, 2019'),
 });
